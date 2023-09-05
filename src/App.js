4e92580aa52296ef5d378e4924728bf4ec978ad2
nicole <125519306+Nickyyyy99@users.noreply.github.com>
2023-09-04 21:51:22 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RouterPrincipal from './Routes/RouterPrincipal';
+
+function App() {
+  return (
+    <div>
+        <RouterPrincipal  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Views/Componente1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Componente1() {
+  return (
+    <div>
+        <h2>Componente 1</h2>
+        <p>Este es el componente 1</p>
+    </div>
+  )
+}
